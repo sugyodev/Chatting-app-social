c223bf639762cd20a13c7b1a7cc86ba8b77a330c
sugyo <sg.guru1030@gmail.com>
2022-08-15 22:39:00 +1000

    store-alert-reducer

--- social-chatting/frontend/src/store/alert/reducer.js
+++ social-chatting/frontend/src/store/alert/reducer.js
@@ -1,22 +1,22 @@
-// import {SET_ALERT, REMOVE_ALERT} from './types' 
+import {SET_ALERT, REMOVE_ALERT} from './types' 
 
-// const initialState = []
+const initialState = []
 
-// const alertReducer =  (state=initialState, action) => {
-//     const {type, payload} = action
+const alertReducer =  (state=initialState, action) => {
+    const {type, payload} = action
 
-//     switch (type) {
-//         case SET_ALERT: return [
-//             ...state,
-//             payload
-//         ]
+    switch (type) {
+        case SET_ALERT: return [
+            ...state,
+            payload
+        ]
 
-//         case REMOVE_ALERT: return state.filter(alert => alert.id !== payload.id)
+        case REMOVE_ALERT: return state.filter(alert => alert.id !== payload.id)
         
     
-//         default: return state
-//     }
+        default: return state
+    }
     
-// }
+}
 
-// export default alertReducer
+export default alertReducer
