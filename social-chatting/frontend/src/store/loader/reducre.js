4fbb8160c56c24eb1de2a5fe5826f8af07e386e6
sugyo <sg.guru1030@gmail.com>
2022-08-18 22:41:42 +1000

    loder-reducer-headlod

--- social-chatting/frontend/src/store/loader/reducre.js
+++ social-chatting/frontend/src/store/loader/reducre.js
@@ -1,27 +1,27 @@
-import { SHOW_LOADER, HIDE_LOADER } from './type'
+// import { SHOW_LOADER, HIDE_LOADER } from './type'
 
-const initialState = {
-    loader: false
-}
+// const initialState = {
+//     loader: false
+// }
 
 
-const loaderReducer = (state=initialState, action) => {
-    const { type } = action
-    switch (type) {
-        case SHOW_LOADER: return {
-            ...state,
-            loader: true
-        }
-        case HIDE_LOADER: return {
-            ...state,
-            loader: false
-        }
+// const loaderReducer = (state=initialState, action) => {
+//     const { type } = action
+//     switch (type) {
+//         case SHOW_LOADER: return {
+//             ...state,
+//             loader: true
+//         }
+//         case HIDE_LOADER: return {
+//             ...state,
+//             loader: false
+//         }
     
-        default: return state
-    }
+//         default: return state
+//     }
     
-}
+// }
 
 
-export default loaderReducer
+// export default loaderReducer
 
