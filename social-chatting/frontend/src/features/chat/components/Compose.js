c3e1dbc4d0e4ca464cfd0f0c1b360ca3fdc91fcc
sugyo <sg.guru1030@gmail.com>
2022-08-23 22:47:26 +1000

    git chat table composer config.js

--- social-chatting/frontend/src/features/chat/components/Compose.js
+++ social-chatting/frontend/src/features/chat/components/Compose.js
@@ -28,13 +28,13 @@ export default function Compose() {
         e.preventDefault();
         dispatch(showLoader());
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `JWT ${localStorage.getItem('access')}`,
-            }
-        };
+        // const config = {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Accept': 'application/json',
+        //         'Authorization': `JWT ${localStorage.getItem('access')}`,
+        //     }
+        // };
         const user = auth.user.email;
         const data = JSON.stringify({user, recipient, title, body});
         axios.post(`${REACT_APP_API_URL}/api/chats/`, data, config)
@@ -64,13 +64,13 @@ export default function Compose() {
                                        value={title}
                                        fullWidth
                             />
-                        // </Grid>
-                        // <Grid item xs={6}>
-                        //     <TextField label="Email" placeholder="Email" name="recipient" variant="outlined"
-                        //                onChange={changeFormData}
-                        //                value={recipient}
-                        //                fullWidth/>
-                        // </Grid>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField label="Email" placeholder="Email" name="recipient" variant="outlined"
+                                       onChange={changeFormData}
+                                       value={recipient}
+                                       fullWidth/>
+                        </Grid>
                         <Grid item xs={12}>
                             <TextField
                                 label="Your Body"
