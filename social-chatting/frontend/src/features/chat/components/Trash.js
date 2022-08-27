4d0ac04586a7ea5f16969eaad65fdbfab8c8338d
sugyo <sg.guru1030@gmail.com>
2022-08-27 23:04:29 +1000

    trash commit 1

--- social-chatting/frontend/src/features/chat/components/Trash.js
+++ social-chatting/frontend/src/features/chat/components/Trash.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import Table from './ChatTable/Table'
-import REACT_APP_API_URL from '../../../testurl'
-import axios from 'axios'
-import { useSelector } from 'react-redux'
+// import React, { useEffect, useState } from 'react'
+// import Table from './ChatTable/Table'
+// import REACT_APP_API_URL from '../../../testurl'
+// import axios from 'axios'
+// import { useSelector } from 'react-redux'
 
-export default function Trash() {
-    const [tableData, setTableData] = useState([])
-    const reload = useSelector(state => state.reload)
+// export default function Trash() {
+//     const [tableData, setTableData] = useState([])
+//     const reload = useSelector(state => state.reload)
 
-    const config = {
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': `JWT ${localStorage.getItem('access')}`,
-        }
-    }; 
-    useEffect(() => {
+//     const config = {
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json',
+//             'Authorization': `JWT ${localStorage.getItem('access')}`,
+//         }
+//     }; 
+//     useEffect(() => {
 
         
-        axios.get(`${REACT_APP_API_URL}/api/chats/getrash/`, config)
-        .then((response) => {
-            setTableData(response.data)
-        })
+//         axios.get(`${REACT_APP_API_URL}/api/chats/getrash/`, config)
+//         .then((response) => {
+//             setTableData(response.data)
+//         })
 
-    }, [reload])
-    return (
-        <>
-            <Table table='trash' tableData={tableData} />
-        </>
-    )
-}
+//     }, [reload])
+//     return (
+//         <>
+//             <Table table='trash' tableData={tableData} />
+//         </>
+//     )
+// }
