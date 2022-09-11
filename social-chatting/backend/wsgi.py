4b4e36764f272a176a0285f1c27dd949c4484818
sugyo <sg.guru1030@gmail.com>
2022-09-11 23:14:12 +1000

    be-first

--- social-chatting/backend/wsgi.py
+++ social-chatting/backend/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for core project.
+# """
+# WSGI config for core project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+# """
 
-import os
+# import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
